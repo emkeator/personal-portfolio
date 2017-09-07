27481e1139a459338aa643a55c2b43f769143ac6
emkeator <emkeator@gmail.com>
2017-09-06 20:48:04 -0600

    fixed footer link to resume

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ import linkedin from './../../images/linkedin.svg';
 import resume from './../../images/resume.svg';
 import email from './../../images/email.svg';
 import instagram from './../../images/instagram.svg';
-import resumeFile from './../../files/EmKeatorResume.pdf';
+import resumeFile from './../../files/EmKeatorTechRes_Sept6.pdf';
 
 
 class Footer extends Component {
@@ -16,7 +16,7 @@ class Footer extends Component {
             <main id="Footer">
                 <a target="_blank" href="https://github.com/emkeator"><img src={github} alt="emily keator github"/></a>
                 <a target="_blank" href="https://www.linkedin.com/in/emkeator/"><img src={linkedin} alt="emily keator linkedin"/></a>
-                <a target="_blank" href={resume} ><img src={resume} alt="emily keator resume"/></a>
+                <a target="_blank" href={resumeFile} ><img src={resume} alt="emily keator resume"/></a>
                 <a target="_blank" href="mailto:emkeator@gmail.com"><img src={email} alt="emily keator email"/></a>              
                 <a target="_blank" href="https://www.instagram.com/emkeator/"><img src={instagram} alt="emily keator instagram"/></a>          
             </main>
